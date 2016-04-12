67ab12f8df296726191f41a42e1c9773ef81a48a
Daria Romanova <romanovadi@gmail.com>
2016-04-12 17:58:52 +0300

    Chenged for any processors quantity
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- lo22/crutch_for_region.sh
+++ lo22/crutch_for_region.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-ls=`ls -1 processor0 | grep '^[0-9].\|^[0-9]'`
-for arg1 in 0 1 2 3 4 5 6 7
+ls1=`ls -1 processor0 | grep '^[0-9].\|^[0-9]'`
+ls2=`ls -1 | grep processor`
+for arg1 in $ls2
 do
-for arg2 in $ls
+for arg2 in $ls1
 do
-cp processor$arg1/0/region processor$arg1/$arg2/region
+cp $arg1/0/region $arg1/$arg2/region
 done
 done
