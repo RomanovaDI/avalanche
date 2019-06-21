ca59a3b9da418b8d3337e96688014bd58292c9d1
Daria Romanova <romanovadi@gmail.com>
2019-06-21 19:32:39 +0300

    No edges in blockMeshDict
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- asc2dict.py
+++ asc2dict.py
@@ -47,13 +47,6 @@ if line_list[0] != 'NODATA_value':
 	exit()
 NODATA_value = float(line_list[1].replace(",", "."))
 
-print(ncols)
-print(nrows)
-print(xllcorner)
-print(yllcorner)
-print(cellsize)
-print(NODATA_value)
-
 print("Write new cell size if necessary:")
 new_cellsize = input()
 if new_cellsize == "":
@@ -63,49 +56,164 @@ else:
 
 import numpy as np
 altitude = np.loadtxt(file_map, dtype=np.str)
+file_map.close()
 altitude = np.char.replace(altitude, ',', '.').astype(np.float64)
-with np.nditer(altitude, op_flags=['readwrite']) as it:
-	for x in it:
-		if x == NODATA_value:
-			x[...] = 0
 print(altitude)
+print(altitude[100, 100])
 
 altitude_mask = np.copy(altitude)
-with np.nditer(altitude_mask, op_flags=['readwrite']) as it:
-	for x in it:
-		if x > 0:
-			x[...] = 1
+f = lambda a: 0 if a == NODATA_value else 1
+fv = np.vectorize(f)
+altitude_mask = fv(altitude_mask)
+print(altitude_mask)
+print(altitude_mask[100, 100])
 
 from scipy import interpolate
 x = np.arange(0, cellsize * ncols, cellsize)
 y = np.arange(0, cellsize * nrows, cellsize)
 xnew = np.arange(0, cellsize * ncols, new_cellsize)
 ynew = np.arange(0, cellsize * nrows, new_cellsize)
-f = interpolate.interp2d(x, y, altitude, kind='cubic')
+f = interpolate.interp2d(x, y, altitude, kind='linear')
 altitude_interpolation = f(xnew, ynew)
 f = interpolate.interp2d(x, y, altitude_mask, kind='linear')
 altitude_interpolation_mask = f(xnew, ynew)
-with np.nditer(altitude_interpolation_mask, op_flags=['readwrite']) as it:
-	for x in it:
-		if x < 1:
-			x[...] = 0
-		else:
-			x[...] = 1
+f = lambda a: 0 if a < 1 else 1
+fv = np.vectorize(f)
+altitude_interpolation_mask = fv(altitude_interpolation_mask)
 altitude_interpolation = altitude_interpolation * altitude_interpolation_mask
-print(xnew.shape)
-print(ynew.shape)
-xnew, ynew = np.meshgrid(xnew, ynew)
-print(xnew.shape)
-print(ynew.shape)
-print(altitude_interpolation.shape)
-print(altitude_interpolation_mask.shape)
+ny = xnew.shape[0]
+nx = ynew.shape[0]
+dx = new_cellsize
+print(altitude_interpolation)
+print(altitude_interpolation[500, 500])
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-#dots = ax.scatter(xnew, ynew, altitude_interpolation_mask)
-surf = ax.plot_surface(xnew, ynew, altitude_interpolation)
-plt.show()
+import math
+f = lambda a: math.floor(a / dx) * dx if a != 0 else NODATA_value
+fv = np.vectorize(f)
+altitude_interpolation = fv(altitude_interpolation)
+print(altitude_interpolation)
+print(altitude_interpolation[500, 500])
 
-file_map.close()
+#maximum = np.amax(altitude_interpolation)
+#with np.nditer(altitude_interpolation, op_flags=['readwrite']) as it:
+#	for x in it:
+#		if x == NODATA_value:
+#			x[...] = maximum + 1000
+#minimum = np.amin(altitude_interpolation)
+#with np.nditer(altitude_interpolation, op_flags=['readwrite']) as it:
+#	for x in it:
+#		if x == maximum + 1000:
+#			x[...] = NODATA_value
+#nz = (maximum - minimum + 50) / dx
+#blocks = np.zeros((nx, ny, nz))
+#it = np.nditer(blocks, flags=['multi_index'], op_flags=['writeonly'])
+#with it:
+#	while not it.finished:
+#		cell_alt = it.multi_index[2] * dx
+#		curr_alt = altitude_interpolation[it.multi_index[0], it.multi_index[1]]
+#		if cell_alt >= surr_alt and cell_alt < curr_alt + 50
+#			it[0] = 1
+#		it.iternext()
+#vertices = np.zeros((nx + 1, ny + 1, nz + 1))
+#it = np.nditer(vertices, flags=['multi_index'], op_flags=['writeonly'])
+#with it:
+#	while not it.finished:
+#		ind = it.multi_index
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (1, 0, 0)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (0, 1, 0)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (0, 0, 1)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (1, 1, 0)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (1, 0, 1)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (0, 1, 1)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		ind = it.multi_index - (1, 1, 1)
+#		if 0 <= ind[0] < nx and 0 <= ind[1] < ny and 0 <= ind[2] < nz:
+#			if blocks[ind] == 1
+#				it[0] = 1
+#		it.iternext()
+
+blockMeshDictFileName = "blockMeshDict"
+file_blockMeshDict = open(blockMeshDictFileName, "w")
+file_blockMeshDict.write("FoamFile\n")
+file_blockMeshDict.write("{\n")
+file_blockMeshDict.write("    version     2.0;\n")
+file_blockMeshDict.write("    format      ascii;\n")
+file_blockMeshDict.write("    class       dictionary;\n")
+file_blockMeshDict.write("    object      blockMeshDict;\n")
+file_blockMeshDict.write("}\n")
+file_blockMeshDict.write("convertToMeters ")
+file_blockMeshDict.write(str(dx))
+file_blockMeshDict.write(";\n")
+file_blockMeshDict.write("vertices\n")
+file_blockMeshDict.write("(\n")
+hight = 50
+hight = math.floor(hight / dx) * dx
+it = np.nditer(altitude_interpolation, flags=['multi_index'], op_flags=["readonly"])
+while not it.finished:
+	if it[0] != NODATA_value:
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx, it.multi_index[1] * dx, it[0]))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx + dx, it.multi_index[1] * dx, it[0]))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx + dx, it.multi_index[1] * dx + dx, it[0]))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx, it.multi_index[1] * dx + dx, it[0]))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx, it.multi_index[1] * dx, it[0] + hight))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx + dx, it.multi_index[1] * dx, it[0] + hight))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx + dx, it.multi_index[1] * dx + dx, it[0] + hight))
+		file_blockMeshDict.write("\t(%f\t%f\t%f)\n" % (it.multi_index[0] * dx, it.multi_index[1] * dx + dx, it[0] + hight))
+	it.iternext()
+file_blockMeshDict.write(");\n")
+file_blockMeshDict.write("blocks\n")
+file_blockMeshDict.write("(\n")
+ind = 0
+it = np.nditer(altitude_interpolation, flags=['multi_index'], op_flags=["readonly"])
+while not it.finished:
+	if it[0] != NODATA_value:
+		file_blockMeshDict.write("\thex (%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d)\t(1 1 %d) simpleGrading (1 1 1)\n" % (ind, ind+1, ind+2, ind+3, ind+4, ind+5, ind+6, ind+7, int(hight / dx)))
+		ind += 8
+	it.iternext()
+file_blockMeshDict.write(");\n")
+file_blockMeshDict.write("edges\n")
+file_blockMeshDict.write("(\n")
+file_blockMeshDict.write(");\n")
+file_blockMeshDict.write("boundary\n")
+file_blockMeshDict.write("(\n")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.write("")
+file_blockMeshDict.close()
+
+
+#from mpl_toolkits.mplot3d import Axes3D
+#import matplotlib.pyplot as plt
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+#xnew, ynew = np.meshgrid(xnew, ynew)
+#dots = ax.scatter(xnew, ynew, altitude_interpolation)
+##surf = ax.plot_surface(xnew, ynew, altitude_interpolation)
+#plt.show()
+
