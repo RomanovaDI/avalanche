516b53a59fb93ca6d7da32fa130f036b42942ca1
Daria Romanova <romanovadi@gmail.com>
2021-06-09 18:35:57 +0300

    Velocity profile writing and oter boundary condition
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- preAndPostProcessing/setUProfileInlet.py
+++ preAndPostProcessing/setUProfileInlet.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dfInlet = pd.read_csv('180321inlet.csv', delimiter="\t", header=None)
+dfInlet = pd.read_csv('data/180321inletSmoothProfile.csv', delimiter="\t", header=None)
+HAlphaWater = 0
+with open('data/180321H.dat') as file:
+	HAlphaWater = float(file.read())
 #dfOutlet = pd.read_csv('180321_outlet.csv', delimiter="\t", header=None)
 #X = dfInlet[0].to_numpy()
 #A = X.reshape((1, np.size(X)))
@@ -24,6 +27,7 @@ V = dfInlet[1].to_numpy()
 #plt.plot(V, Z, label="interp")
 #plt.show()
 fU = open("constant/boundaryData/leftInletWall/0/U", "w")
+fAlphaWater = open("constant/boundaryData/leftInletWall/0/alpha.water.orig", "w")
 fPoints = open("constant/boundaryData/leftInletWall/points", "w")
 Ystart = 0.0
 Yfinish = 0.005
@@ -32,6 +36,7 @@ Y = np.arange(Ystart, Yfinish + Ystep, Ystep)
 nPoints = Y.shape[0] * Z.shape[0] + 1
 fU.write("%d\n(\n\t(0.0\t0.0\t0.0)\n" % (nPoints))
 fPoints.write("%d\n(\n\t(0.0\t%f\t0.0)\n" % (nPoints, Y[1]))
+fAlphaWater.write("%d\n(\n\t1.0\n" % (nPoints))
 for y in Y:
 	if y == Y[0]:
 		fU.write(("\t(0.0\t0.0\t0.0)\n") * Z.shape[0])
@@ -41,8 +46,14 @@ for y in Y:
 		for v in V:
 			fU.write("\t(%f\t0.0\t0.0)\n" % (v))
 	for z in Z:
-		fPoints.write("\t(0.0\t%f\t%f)\n" % (y, z))
+		fPoints.write("\t(0.0\t%f\t%f)\n" % (y, z*0.001))
+		if z <= HAlphaWater:
+			fAlphaWater.write("\t%f\n" % (1.0))
+		else:
+			fAlphaWater.write("\t%f\n" % (0.0))
 fPoints.write(")")
 fU.write(")")
+fAlphaWater.write(")")
 fPoints.close()
 fU.close()
+fAlphaWater.close()
